4ccf9a77057b2e3484e93ab2185bc7a0b521a22c
dadandr <146108340+dadandr@users.noreply.github.com>
2023-10-09 16:58:05 +0400

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,15 @@
+father = 51
+mother = 42
+brother1 = 23
+brother2 = 22
+sister = 18
+grandfather = 89
+grandmother = 82
+
+print(father  + 5)
+print(mother  + 5)
+print(brother1  + 5)
+print(brother2  + 5)
+print(sister + 5)
+print(grandfather + 5)
+print(grandmother + 5)
